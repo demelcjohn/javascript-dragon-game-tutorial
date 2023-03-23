1be2a822051172a7ae63a645c2de70a3c9d24d08
Demel C <98221839+demelcjohn@users.noreply.github.com>
2023-03-23 13:28:51 +0000

    update 3

--- script.js
+++ script.js
@@ -17,6 +17,25 @@ const monsterStats = document.querySelector("#monsterStats");
 const monterNameText = document.querySelector("#monterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+const weapons = [
+  {
+    name: "stick",
+    power: 5
+  },
+  {
+    name: "dagger",
+    power: 30
+  },
+  {
+    name: "claw hammer",
+    power: 50
+  },
+  {
+    name: "sword",
+    power: 100
+  }
+];
+
 const locations = [
   {
     name:"town square",
@@ -32,8 +51,8 @@ const locations = [
   },
   {
     name:"cave",
-    "button text": ["Flight slime","Flight fanged beast","Go to town square"],
-    "button functions":[flightSlime, flightBeast, goTown],
+    "button text": ["Fight slime","Fight fanged beast","Go to town square"],
+    "button functions":[fightSlime, fightBeast, goTown],
     text: "You enter the cave. You see some monsters."
   }
 ]
@@ -71,14 +90,40 @@ function fightDragon(){
 }
 
 function buyHealth(){
-  
+  if(gold>=10){
+  gold = gold - 10
+  health += 10
+  goldText.innerText = gold
+  healthText.innerText = health
+  }
+  else{
+    text.innerText = "You do not have enough gold to buy health."
+  }
 }
 
 function buyWeapon(){
-  
+  if(currentWeapon<weapons.length-1)
+  {
+  if(gold>=30){
+    gold-=30
+    currentWeapon++
+    goldText.innerText = gold;
+    let newWeapon = weapons[currenWeapon].name
+    text.innerText = "You now have a "+ newWeapon+"."
+    inventory.push(newWeapon)
+    text.innerText += "In your inventory you have : "+inventory
+  }
+  else
+    text.innerText = "You do not have enough gold to buy  a weapon."
+  }
+  else{
+    text.innerText = "You already have the most powerful weapon!"
+    button2.innerText = "Sell weapon for 15 gold"
+    button2.onclick = sellWeapon
+  }
 }
 
-function flightSlime(){
+function fightSlime(){
   
 }
 
