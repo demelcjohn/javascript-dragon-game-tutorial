f234c9842d13f44c174ad1b5ba136cf4c0f5c099
Demel C <98221839+demelcjohn@users.noreply.github.com>
2023-03-23 18:03:01 +0000

    update 4

--- script.js
+++ script.js
@@ -78,6 +78,18 @@ const locations = [
     "button text": ["Attack","Dodge","Run"],
     "button functions":[attack, dodge, goTown],
     text: "You are fighting a monster."
+  },
+  {
+    name:"kill monster",
+    "button text": ["Go to town square","Go to town square","Go to town square"],
+    "button functions":[goTown, goTown, goTown],
+    text: 'The monster screams "Arh!!" as it dies. You gain experience points and find gold.'
+  },
+  {
+    name:"lose",
+    "button text": ["REPLAY","REPLAY","REPLAY"],
+    "button functions":[restart,restart,restart],
+    text: "You dies. "
   }
 ]
 
@@ -87,6 +99,7 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location){
+  monsterStats.style.display = "none"
   button1.innerText = location["button text"][0];
   button2.innerText = location["button text"][1];
   button3.innerText = location["button text"][2];
@@ -174,6 +187,41 @@ function fightDragon(){
 }
 
 function goFight(){
+  update(locations[3])
+  monsterHealth = monsters[fighting].health
+  monsterStats.style.display = "block"
+  monsterNameText.innerText= monsters[fighting].name
+  monsterHealth.innerText = monsterHealth
+}
+
+function attack(){
+  text.innerText = "The "+monster[fighting].name+" attacks."
+  text.innerText += "You attack it with your "+weapons[currentWeapon].name+"."
+  health -= monsters[fighting].level
+  monsterHealth -= weapons[currentWeapon].power+Math.floor(Math.random()*xp)+1
+  healthText.innerText = health
+  monsterHealthText.innerText = monsterHealth
+  if(health<=0){
+    lose()
+  }
+  else if(monsterHealth<=0){
+    defeatMonster()
+  }
+}
+
+function dodge(){
+  text.innerText = "You dodge the attack from the "+monster[fighting].name+"."
+}
+
+function defeatMonster(){
+  gold +=Math.floor(monsters[fighting].level*6.7)
+  xp += monsters[fighting].level
+  goldText.innerText = gold
+  xpText.innerText = xp
+  update(locations[4])
+}
+
+function lose(){
   
 }
 
